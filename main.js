988746aa6c928fc6214cb69931661ba781ee1817
Muhammad Qalby <qolbymuhammad86@gmail.com>
2024-06-22 20:08:06 +0800

    First commit

--- main.js
+++ main.js
@@ -1,200 +1,204 @@
+// require("dotenv").config();
+
 let WALLET_CONNECTED = "";
-let contractAddress = "0x1ccB0A73939F3d224f3a7e5EdB85415dF90b04E8";
+// let contractAddress = "0x1ccB0A73939F3d224f3a7e5EdB85415dF90b04E8";
+let contractAddress = "0x2F012Efe614512C67b6312E55F1B145d41194249";
+// let contractAddress = process.env.CONTRACT_ADDRESS;
 let contractAbi = [
     {
-      "inputs": [
-        {
-          "internalType": "string[]",
-          "name": "_candidateNames",
-          "type": "string[]"
-        },
-        {
-          "internalType": "uint256",
-          "name": "_durationInMinutes",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_name",
-          "type": "string"
-        }
-      ],
-      "name": "addCandidate",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "name": "candidates",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "name",
-          "type": "string"
-        },
-        {
-          "internalType": "uint256",
-          "name": "voteCount",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "getAllVotesOfCandiates",
-      "outputs": [
-        {
-          "components": [
+        inputs: [
             {
-              "internalType": "string",
-              "name": "name",
-              "type": "string"
+                internalType: "string[]",
+                name: "_candidateNames",
+                type: "string[]",
             },
             {
-              "internalType": "uint256",
-              "name": "voteCount",
-              "type": "uint256"
-            }
-          ],
-          "internalType": "struct Voting.Candidate[]",
-          "name": "",
-          "type": "tuple[]"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+                internalType: "uint256",
+                name: "_durationInMinutes",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "nonpayable",
+        type: "constructor",
     },
     {
-      "inputs": [],
-      "name": "getRemainingTime",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+        inputs: [
+            {
+                internalType: "string",
+                name: "_name",
+                type: "string",
+            },
+        ],
+        name: "addCandidate",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
     },
     {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_candidateIndex",
-          "type": "uint256"
-        }
-      ],
-      "name": "getVotesOfCandiate",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        name: "candidates",
+        outputs: [
+            {
+                internalType: "string",
+                name: "name",
+                type: "string",
+            },
+            {
+                internalType: "uint256",
+                name: "voteCount",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
     },
     {
-      "inputs": [],
-      "name": "getVotingStatus",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+        inputs: [],
+        name: "getAllVotesOfCandiates",
+        outputs: [
+            {
+                components: [
+                    {
+                        internalType: "string",
+                        name: "name",
+                        type: "string",
+                    },
+                    {
+                        internalType: "uint256",
+                        name: "voteCount",
+                        type: "uint256",
+                    },
+                ],
+                internalType: "struct Voting.Candidate[]",
+                name: "",
+                type: "tuple[]",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
     },
     {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "_candidateIndex",
-          "type": "uint256"
-        }
-      ],
-      "name": "vote",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
+        inputs: [],
+        name: "getRemainingTime",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
     },
     {
-      "inputs": [
-        {
-          "internalType": "address",
-          "name": "",
-          "type": "address"
-        }
-      ],
-      "name": "voters",
-      "outputs": [
-        {
-          "internalType": "bool",
-          "name": "",
-          "type": "bool"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "_candidateIndex",
+                type: "uint256",
+            },
+        ],
+        name: "getVotesOfCandiate",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
     },
     {
-      "inputs": [],
-      "name": "votingEnd",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
+        inputs: [],
+        name: "getVotingStatus",
+        outputs: [
+            {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
     },
     {
-      "inputs": [],
-      "name": "votingStart",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "_candidateIndex",
+                type: "uint256",
+            },
+        ],
+        name: "vote",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+    },
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        name: "voters",
+        outputs: [
+            {
+                internalType: "bool",
+                name: "",
+                type: "bool",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "votingEnd",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "votingStart",
+        outputs: [
+            {
+                internalType: "uint256",
+                name: "",
+                type: "uint256",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+];
 
-const connectMetamask = async() => {
+const connectMetamask = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
     WALLET_CONNECTED = await signer.getAddress();
     var element = document.getElementById("metamasknotification");
-    element.innerHTML = "Metamask is connected " + WALLET_CONNECTED;
-}
+    element.innerHTML = "Metamask telah terhubung: " + WALLET_CONNECTED;
+};
 
-const addVote = async() => {
-    if(WALLET_CONNECTED != 0) {
+const addVote = async () => {
+    if (WALLET_CONNECTED != 0) {
         var name = document.getElementById("vote");
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         await provider.send("eth_requestAccounts", []);
@@ -205,15 +209,14 @@ const addVote = async() => {
         const tx = await contractInstance.vote(name.value);
         await tx.wait();
         cand.innerHTML = "Vote added !!!";
-    }
-    else {
+    } else {
         var cand = document.getElementById("cand");
-        cand.innerHTML = "Please connect metamask first";
+        cand.innerHTML = "Hubungkan akun ke metamask terlebih dahulu";
     }
-}
+};
 
-const voteStatus = async() => {
-    if(WALLET_CONNECTED != 0) {
+const voteStatus = async () => {
+    if (WALLET_CONNECTED != 0) {
         var status = document.getElementById("status");
         var remainingTime = document.getElementById("time");
         const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -225,15 +228,14 @@ const voteStatus = async() => {
         console.log(time);
         status.innerHTML = currentStatus == 1 ? "Voting is currently open" : "Voting is finished";
         remainingTime.innerHTML = `Remaining time is ${parseInt(time, 16)} seconds`;
-    }
-    else {
+    } else {
         var status = document.getElementById("status");
-        status.innerHTML = "Please connect metamask first";
+        status.innerHTML = "Hubungkan akun ke metamask terlebih dahulu";
     }
-}
+};
 
-const getAllCandidates = async() => {
-    if(WALLET_CONNECTED != 0) {
+const getAllCandidates = async () => {
+    if (WALLET_CONNECTED != 0) {
         var p3 = document.getElementById("p3");
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         await provider.send("eth_requestAccounts", []);
@@ -255,10 +257,10 @@ const getAllCandidates = async() => {
             statusCell.innerHTML = candidates[i].voteCount;
         }
 
-        p3.innerHTML = "The tasks are updated"
-    }
-    else {
+        // p3.innerHTML = "The tasks are updated";
+        p3.innerHTML = "";
+    } else {
         var p3 = document.getElementById("p3");
-        p3.innerHTML = "Please connect metamask first";
+        p3.innerHTML = "Hubungkan akun ke metamask terlebih dahulu";
     }
-}
+};
